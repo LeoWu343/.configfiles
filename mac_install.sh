00193df784e139cc63a530ca326abf73f1b56248
Leo Wu <leowu.343@gmail.com>
2019-03-18 14:40:38 -0700

    macvim override system vim flag seems to be applied by default now

--- mac_install.sh
+++ mac_install.sh
@@ -37,8 +37,7 @@ sudo xcode-select -s /Applications/Xcode.app/Contents/Developer
 # install packages
 # tools: macvim, zsh, git, tmux, fasd
 # dependencies: reattach-to-user-namespace (vim copy and paste), cmake (vim plugin YouCompleteMe), ctags (vim plugin Tagbar)
-brew install macvim --with-override-system-vim
-brew install zsh git tmux fasd ack reattach-to-user-namespace cmake ctags
+brew install macvim zsh git tmux fasd ack reattach-to-user-namespace cmake ctags
 # get brew rmtree
 brew tap beeftornado/rmtree
 # install programs
